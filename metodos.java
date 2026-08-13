ba4933fa22954053dd6be4da6b9e55a1fb2870e0
EDWAR <313447113+Edwar115TH1@users.noreply.github.com>
2026-08-12 20:45:25 -0500

    Implement MostrarDiagonalSuperior method
    Added a method to display the upper diagonal of a matrix.

--- metodos.java
+++ metodos.java
@@ -16,6 +16,16 @@ public class metodos {
             System.out.println();
         }
     }
+     public void MostrarDiagonalSuperior(ObjMatriz[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                if (i == j) {
+                    System.out.print(" - " + m[i][j].getDato());
+                }
+            }
+            System.out.println();
+        }
+    }
 
     public ObjMatriz[][] LlenarMatrizObjetual(ObjMatriz[][] m) {
         for (int i = 0; i < m.length; i++) {
